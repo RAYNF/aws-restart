a8f653d387737965f3ae87bd17fb3d9403e2eee7
RAYNF <123804826+RAYNF@users.noreply.github.com>
2025-12-30 20:06:42 +0700

    learning uploading github

---
+++ categorize-values.py
@@ -0,0 +1,6 @@
+myFixedTypeList = [45, 290578, 1.02, True, "My dog is on the bed.", "45",]
+
+for item in myFixedTypeList:
+    print("{} is of the data type {}".format(item,type(item)))
+    
+    
---
+++ collections_test.py
@@ -0,0 +1,44 @@
+myFruitList = ["apple","banana","cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+
+print(myFruitList[0])
+
+print(myFruitList[1])
+
+print(myFruitList[2])
+
+myFruitList[2] = "orange"
+
+print(myFruitList)
+
+myFinalAnswearTuple = ("apple","banana","pineaplle")
+print(myFinalAnswearTuple)
+
+print(type(myFinalAnswearTuple))
+
+print(myFinalAnswearTuple[0])
+
+print(myFinalAnswearTuple[1])
+
+print(myFinalAnswearTuple[2])
+
+myFavoriteFruitDictionary = {
+    "akua": "apple",
+    "saanvi":"banana",
+    "paulo": "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+
+print(type(myFavoriteFruitDictionary))
+
+print(myFavoriteFruitDictionary["akua"])
+
+print(myFavoriteFruitDictionary["saanvi"])
+
+print(myFavoriteFruitDictionary["paulo"])
+
+
+
